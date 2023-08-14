7151241099a81e521b532e2c69621bb6ccd31dd6
Lester <lestersantos271@gmail.com>
2023-08-14 14:32:53 -0600

    Adding templates

--- Poll_App/polls/views.py
+++ Poll_App/polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 """
@@ -7,8 +8,11 @@ an has a specific template.
 """
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("detail You're looking at question %s." % question_id)
