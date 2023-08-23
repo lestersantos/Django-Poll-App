e80fe6e4e961962ff060b9b9dafab8254b75032c
Lester <lestersantos271@gmail.com>
2023-08-23 14:03:58 -0600

    Customizing the admin change list

--- Poll_App/polls/models.py
+++ Poll_App/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 
 # Create your models here.
 """
@@ -15,10 +16,15 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-    
+
     """
     Fixing the bug Question.was_published_recently() by overriding? maybe
     """
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    ) 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
