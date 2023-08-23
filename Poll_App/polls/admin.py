d7f9d866ec0a8d89006cc176e7a4ee7e02ae0c37
Lester <lestersantos271@gmail.com>
2023-08-23 11:44:41 -0600

    Customize admin form and adding related objects

--- Poll_App/polls/admin.py
+++ Poll_App/polls/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from .models import Question
-# Register your models here.
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+"""
+Firs element of each tuple is fieldsets is the title of the fieldset
+"""
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {"fields": ["question_text"]})\
+                 ,("Date information", {"fields": ["pub_date"]}),\
+                ]
+    inlines = [ChoiceInline]
+
+
+# Register your models here.
+admin.site.register(Question, QuestionAdmin)
